0c6404d027a7733af9570aaa496af51d8a22567d
ShafathZ <shafath.zaman@gmail.com>
2026-04-20 23:12:53 -0400

    Added initial template files for eval and model portions

---
+++ evaluations.py
@@ -0,0 +1,5 @@
+
+
+# TODO: Input a reddit message thread and output a Tuple of evaluation results
+def get_evaluations_from_post():
+    pass
---
+++ model.py
@@ -0,0 +1,14 @@
+
+class DetoxifyModel:
+    # TODO: Import model and use detoxify algorithm
+    def __init__(self):
+        pass
+
+    # TODO: Algorithm definition
+    def detoxify(self, text):
+        pass
+
+class AggregatorModel:
+    # TODO: Design aggregator to take in stats from detoxification and compute score
+    def __init__(self):
+        pass
